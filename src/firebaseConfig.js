637406a23865336bf4deebfd9195c35f2c4e1843
morandj <dominic.moran@gmail.com>
2019-06-11 18:57:44 +0100

    firebase and tailwind install

---
+++ src/firebaseConfig.js
@@ -0,0 +1,20 @@
+import firebase from "firebase";
+
+// firebase init goes here
+const config = {
+  apiKey: "AIzaSyDkWeRkVZjfHTv9ejfjyw11RgyvDlYSLzI",
+  authDomain: "vue-a2.firebaseapp.com",
+  databaseURL: "https://vue-a2.firebaseio.com",
+  projectId: "vue-a2",
+  storageBucket: "vue-a2.appspot.com",
+  messagingSenderId: "364630730437",
+  appId: "1:364630730437:web:30e0b478fcd89860"
+};
+firebase.initializeApp(config);
+
+// firebase utils
+const auth = firebase.auth();
+const db = firebase.firestore();
+const ff = firebase.functions();
+
+export { auth, db, ff };
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
+    <h1 class="text-blue-700">Testing.</h1>
     <img alt="Vue logo" src="../assets/logo.png" />
+
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
